a46f5bf07743f1833ed45562aa02b48e4a692021
awiggin <awiggin1995@gmail.com>
2021-11-05 21:59:17 +0100

    modificando .sh

--- scripts/jenkins_ruta_apli.sh
+++ scripts/jenkins_ruta_apli.sh
@@ -6,12 +6,12 @@ echo "El valor de la variable 1 es "$1""
 echo "El valor de la variable 2 es "$2""
 echo "El valor de la variable log es "$log""
 echo "Creamos directorio lanzando comando mkdir -p $DIR/tmp"
-mkdir -p $DIR/tmp
-echo "Creamos fichero lanzando touch $DIR/tmp/$log"
-touch $DIR/tmp/$log
-exec 2>$DIR/tmp/$log
+mkdir -p '$DIR'/tmp
+echo "Creamos fichero lanzando touch '$DIR'/tmp/'$log'"
+touch '$DIR'/tmp/'$log'
+exec 2>'$DIR'/tmp/'$log'
 echo "Hello World"
-err="$(cat $DIR/tmp/$log | wc -l)"
+err="$(cat '$DIR'/tmp/'$log' | wc -l)"
 if [[ "$err" == 0 ]]
 then 
 	if [[ "$2" == "true" ]]
