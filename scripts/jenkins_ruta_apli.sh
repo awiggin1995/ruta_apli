e7d99f8ff48e47fb4ba6500c0790b1b7ed5f77ce
awiggin <awiggin1995@gmail.com>
2021-11-05 22:36:13 +0100

    modificando .sh

--- scripts/jenkins_ruta_apli.sh
+++ scripts/jenkins_ruta_apli.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DIR="$WORKSPACE"
-log=$DIR/tmp/err.dat.`date +%F-%H%M`
+log=$( $(DIR)/tmp/err.dat.$( date +%F-%H%M))
 echo "El valor de DIR es "$DIR""
 echo "El valor de la variable 1 es "$1""
 echo "El valor de la variable 2 es "$2""
@@ -9,13 +9,13 @@ touch $log
 echo "Hello World"
 if [ -s ! "$log" ]
 then 
-	if [ "${2}" == "true" ]
+	if [ "${2}" -eq "true" ]
 	then
-		echo "El valor de la segunda variable es "$2"."
+		echo "El valor de la segunda variable es $2"
 		echo "Deployamos"
 		exit 0
 	else
-		echo "El valor de la segunda variable es "$2"."
+		echo "El valor de la segunda variable es $2"
 		echo "NO Deployamos"
 		exit 0
 	fi
