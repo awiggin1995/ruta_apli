5838b8c04e56553767ed876b367e917543c8a218
awiggin <awiggin1995@gmail.com>
2021-11-05 21:31:54 +0100

    modificando .sh

--- scripts/jenkins_ruta_apli.sh
+++ scripts/jenkins_ruta_apli.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR='$( cd '$( dirname '${BASH_SOURCE[0]}' )' && pwd )'
+log=err.dat.$(date +%F-%H%M)
+echo "El valor de DIR es $DIR"
 mkdir -p $DIR/$1/tmp
-exec 2>$DIR/$1/tmp/err.dat.$(date +%F-%H%M)
-
+touch $DIR/$1/tmp/$log
+exec 2>$DIR/$1/tmp/$log
 echo "Hello World"
-
-
-
-
-
-
-
-err=$(cat $DIR/$1/tmp/err.dat.$(date +%F-%H%M)|wc -l)
-
+err=$(cat $DIR/$1/tmp/$log | wc -l)
 if [[ "$err" == 0 ]]
 then 
-	exit 0
+	if [[ "$2" == "true" ]]
+	then
+		echo "El valor de la segunda variable es $2."
+		echo "Deployamos"
+		exit 0
+	else
+		echo "El valor de la segunda variable es $2."
+		echo "NO Deployamos"
+		exit 0
+	fi
 else 
 	exit 1
 fi
+
