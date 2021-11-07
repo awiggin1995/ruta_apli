af5e508263d7ef50a35e1004c5cbc9d075b1e604
awiggin <awiggin1995@gmail.com>
2021-11-07 09:26:18 +0100

    Modificando sh

--- scripts/jenkins_ruta_apli.sh
+++ scripts/jenkins_ruta_apli.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-DIR="$WORKSPACE"
-log="$WORKSPACE"/tmp/err.dat.$( date +%F-%H%M)
-echo "El valor de DIR es "$DIR""
-echo "El valor de la variable 1 es "$1""
-echo "El valor de la variable 2 es "$2""
-echo "El valor de la variable log es "$log""
-touch $log
+export WORKSPACE=/home/awiggin/GitHub/ruta_apli/
+export TMP=/home/awiggin/GitHub/ruta_apli/tmp/
+export DIR=$WORKSPACE
+export date=$(date +%F-%H%M)
+export log=${TMP}err.dat.${date}
+touch "${log}"
+echo "El valor de DIR es $DIR"
+echo "El valor de la variable 1 es $1"
+echo "El valor de la variable 2 es $2"
+echo "El valor de la variable log es $log"
 echo "Hello World"
 if [ -s "$log" ]
 then 
@@ -14,4 +17,3 @@ then
 else 
 	echo "No hay errores, finalizamos con exit 0"
 	exit 0
-fi
